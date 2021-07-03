2ec949b23547389f2c34c5d3fef194de99a52abf
Bibi <84073518+xBibie@users.noreply.github.com>
2021-07-03 13:10:54 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,18 @@
+
+const colors = [yellow, blue, red, orange]
+
+while (colors){
+    console.log (colors ++);
+    return colors
+};
+
+for (colors = 0; Array.length ++){
+    console.log (color);
+};
+
+colors.forEach(element => console.log (element));
+
+//3
+//1
+//for Each is kort en krachtig
+// nee
